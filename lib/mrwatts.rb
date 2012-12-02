31fbb83c7a777200653d49c8a795493fa424d049
Christopher Ferris <christopher.d.ferris@gmail.com>
2012-12-02 14:17:04 -0800

    Making the notes system more flexible; adding sequence-based melodies rather than random notes

--- lib/mrwatts.rb
+++ lib/mrwatts.rb
@@ -12,27 +12,40 @@ class Mrwatts
 
 		@octaves = [16, 28, 40, 52, 64, 76, 88, 100, 112, 124]
 
-		puts "Hello, how you be"
-	end
+	 	s = Sequence.new()
+		@note_lengths = {
+			:whole => s.note_to_delta('whole'),
+			:half => s.note_to_delta('half'),
+			:quarter => s.note_to_delta('quarter'),
+			:eighth => s.note_to_delta('eighth')
+		}
 
-	def tell_joke
-		puts "Space."
+		puts "Hello, how you be"
 	end
 
 	def build_melody
 		r = Random.new
 		melody = []
-		length = 16
-		#TODO: make this based on sequences rather than
-		#single random notes.
-		for i in 1 .. length
-			melody << r.rand(8)
-		end
+		#length = 16
+		offsets = [1, 2, 3, 4]
+		seq = random_sequence[0]
+		offsets.each { |offset| 
+			seq.each { |chunk|
+				melody << {:note => chunk[:note] + offset - 1, :length => chunk[:length]}
+			}
+		}
+
 		melody
 	end
 
 	def build_bassline
-		[1, 5, 3, 4]
+		[
+		{:note => 1, :length => @note_lengths[:whole]},
+		{:note => 5, :mod => -1, :length => @note_lengths[:half]},
+		{:note => 5, :length => @note_lengths[:half]},
+		{:note => 3, :length => @note_lengths[:whole]},
+		{:note => 4, :length => @note_lengths[:whole]}
+		]
 	end
 
 	def get_scales
@@ -45,8 +58,101 @@ class Mrwatts
 			:aeolian => [0, 2, 3, 5, 7, 8, 10, 12],
 			:locrian => [0, 1, 3, 5, 6, 8, 10, 12],
 			:harmonic_minor => [0, 2, 3, 5, 7, 8, 11, 12],
-			:melodic_minor => [0, 2, 3, 5, 7, 9, 11, 12]
+			:melodic_minor => [0, 2, 3, 5, 7, 9, 11, 12],
+			:chromatic => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 		}
+	end
+
+	def random_sequence
+		#r = Random.new
+		#Idea: this data could be stored in as json.
+		sequences = [
+			[{:note => 1, :length => @note_lengths[:quarter]},
+			{:note => 2, :length => @note_lengths[:quarter]}, 
+			{:note => 3, :length => @note_lengths[:half]}],
+		]
+		#sequences[r.rand(sequences.length)]
+	end
+
+	def build_track(note_array, track, scale, pitch, chords = false)
+	  	s = @scales[scale]
+		note_array.each do |offset|
+			note = offset[:note]
+			length = offset[:length]
+			mod = offset[:mod] || 0
+
+		  	off = (note - 1) % 8
+		  	oct = @octaves[pitch]
+
+			if chords then
+			  	chord_notes = [s[fix(off)], s[fix(off + 2)], s[fix(off + 4)]]
+			  	track.chord(chord_notes)
+		  	else
+		  		track.events << NoteOn.new(0, oct + s[off] + mod, @velocity, 0)
+		  		track.events << NoteOff.new(0, oct + s[off] + mod, @velocity, length)
+			end
+		end
+	end
+
+
+	def fix(num)
+		if num > 8 then num -= 8 end
+		num
+	end
+
+	def build(scale)
+
+		seq = Sequence.new()
+		track = ReggieTrack.new(seq, @song)
+		seq.tracks << track
+		track.events << Tempo.new(Tempo.bpm_to_mpq(@bpm))
+		track.events << MetaEvent.new(META_SEQ_NAME, @song_name)
+
+		@velocity = 127
+		# Add a volume controller event (optional).
+		track.events << Controller.new(0, CC_VOLUME, @velocity)
+
+		# Add events to the track: a major scale. Arguments for note on and note off
+		# constructors are channel, note, velocity, and delta_time. Channel numbers
+		# start at zero. We use the new Sequence#note_to_delta method to get the
+		# delta time length of a single quarter note.
+		
+		#melody
+		# Create a track to hold the notes. Add it to the sequence.
+		melody_track = ReggieTrack.new(seq, @song)
+		seq.tracks << melody_track
+
+		# Give the track a name and an instrument name (optional).
+		melody_track.name = 'Melody Track'
+		melody_track.instrument = 24
+
+		melody_track.events << ProgramChange.new(0, 1, 0)
+
+		@melody = build_melody
+
+	 	#TODO:make this less awful
+		build_track(@melody, melody_track, scale, 4)
+
+		#bassline
+		bassline_track = ReggieTrack.new(seq, @song)
+		seq.tracks << bassline_track
+
+		bassline_track.name = 'Bassline Track'
+		bassline_track.instrument = 2
+		bassline_track.events << ProgramChange.new(0, 1, 0)
+
+
+		@bassline = build_bassline
+
+		build_track(@bassline, bassline_track, scale, 2)
+
+		File.open("#{@song_name}.mid", 'wb') { | file | seq.write(file) }
+
+		puts "Built."
+	end
+
+	def tell_joke
+		puts "What sound does a squirrel make?"
 	end
 
 	def random_name
@@ -61,84 +167,4 @@ class Mrwatts
 		names[r.rand(names.length)]
 	end
 
-	def random_sequence
-		r = Random.new
-		sequences = [
-			[1, 2, 3],
-			[1, 5, 3]
-		]
-		sequences[r.rand(sequences.length)]
-	end
-
-	def build_track(note_array, track, scale, note_length, pitch)
-		note_array.collect! { |n| n + @octaves[pitch] }
-		note_array.each do |offset|
-		  off = (offset - 1) % 8
-		  oct = offset - off
-		  s = @scales[scale]
-		  track.events << NoteOn.new(0, oct + s[off], @velocity, 0)
-		  track.events << NoteOff.new(0, oct + s[off], @velocity, note_length)
-		  
-		  #track.chord([s[], s[o + 2]])
-		end
-	end
-
-	def build(scale)
-	 	seq = Sequence.new()
-
-		track = ReggieTrack.new(seq, @song)
-		seq.tracks << track
-		track.events << Tempo.new(Tempo.bpm_to_mpq(@bpm))
-		track.events << MetaEvent.new(META_SEQ_NAME, @song_name)
-
-		@velocity = 127
-		# Add a volume controller event (optional).
-		track.events << Controller.new(0, CC_VOLUME, @velocity)
-
-		# Add events to the track: a major scale. Arguments for note on and note off
-		# constructors are channel, note, velocity, and delta_time. Channel numbers
-		# start at zero. We use the new Sequence#note_to_delta method to get the
-		# delta time length of a single quarter note.
-
-		note_lengths = {
-			:whole => seq.note_to_delta('whole'),
-			:half => seq.note_to_delta('half'),
-			:quarter => seq.note_to_delta('quarter'),
-			:eighth => seq.note_to_delta('eighth')
-		}
-		
-		#melody
-		# Create a track to hold the notes. Add it to the sequence.
-		melody_track = ReggieTrack.new(seq, @song)
-		seq.tracks << melody_track
-
-		# Give the track a name and an instrument name (optional).
-		melody_track.name = 'Melody Track'
-		melody_track.instrument = GM_PATCH_NAMES[0]
-
-		melody_track.events << ProgramChange.new(0, 1, 0)
-	 	@melody = build_melody
-
-	 	#TODO:make this less awful
-		build_track(@melody, melody_track, scale, note_lengths[:quarter], 4)
-
-		#bassline
-		bassline_track = ReggieTrack.new(seq, @song)
-		seq.tracks << bassline_track
-		bassline_track.events << ProgramChange.new(0, 1, 0)
-
-		@bassline = build_bassline
-
-		build_track(@bassline, bassline_track, scale, note_lengths[:whole], 2)
-
-		# Calling recalc_times is not necessary, because that only sets the events'
-		# start times, which are not written out to the MIDI file. The delta times are
-		# what get written out.
-		# track.recalc_times
-
-		File.open("#{@song_name}.mid", 'wb') { | file |
-			seq.write(file)
-		}
-		puts "built"
-	end
 end
